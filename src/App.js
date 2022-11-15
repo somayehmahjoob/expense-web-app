cc9580bee6a9596706044e512a84f752c2a686d1
Somayeh mahjoob <somayehmahjob@gmail.com>
2022-11-15 09:36:31 +0000

    [ADD] Card componet -- component for ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './Components/ExpenseItem';
+import Expenses from './Components/Expenses';
 import './App.css';
 
 function App() {
@@ -9,7 +9,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -23,16 +28,10 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
   return (
     <div className="App">
-      <ExpenseItem 
-        title={expenses[0].title} 
-        amount={expenses[0].amount} 
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      <Expenses items = {expenses}/>
     </div>
   );
 }
